ee4f0cf685a3d7b8d0eb459b35a06a4d08dc3e6f
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-03-12 17:07:54 +0530

    Create nor_gate.sv

---
+++ Design_Sources/nor_gate.sv
@@ -0,0 +1,28 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/12/2025 05:00:47 PM
+// Design Name: 
+// Module Name: nor_gate
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module nor_gate(a,b,y);
+    input a; 
+    input b; 
+    output y;
+assign y = ~(a | b);
+endmodule
