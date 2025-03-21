14d610a33bf6f3d0cc4b42fe34940b67d1116362
Prachi Deodatta Gore <77486432+cigi10@users.noreply.github.com>
2025-03-21 15:43:28 +0530

    Create counter.sv

---
+++ Design_Sources/counter.sv
@@ -0,0 +1,39 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 03/16/2025 02:29:56 PM
+// Design Name: 
+// Module Name: counter
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+
+module counter 
+(
+    input logic clk,
+    input logic rst,
+    output logic [3:0] count
+);
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst)
+            count <= 4'b0000;
+        else
+            count <= count + 1;
+    end
+endmodule
+
+
+
